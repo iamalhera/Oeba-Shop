449f97e11937ecbacf80efee0808ca8d948ea69b
Al Hera Ahmad <alheraahmad0786@gmail.com>
2023-03-09 14:35:02 +0530

    feat : added dark mode toggle switch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
-import {Header} from "./components";
-import {Cart, ProductListing, Wishlist, Homepage, NotFound } from "./pages";
+import { Header } from "./components";
+import { Cart, ProductListing, Wishlist, Homepage, NotFound } from "./pages";
 import useLocalStorage from 'use-local-storage';
 import { Route, Routes } from 'react-router-dom';
+import Profile from './components/Profile';
 
 function App() {
   const defaultDark = window.matchMedia("(prefers-color-scheme : dark)").matches;
@@ -17,9 +18,20 @@ function App() {
   return (
     <div className="App" data-theme={theme}>
       <Header />
-      <button className="btn btn-primary-outline switch-btn" onClick={switchTheme}>
-        Change Theme
-      </button>
+      <div className="toggle-div">
+        <label className="toggle-switch">
+          <input
+            checked={theme === "dark" ? true : false}
+            onChange={switchTheme}
+            type="checkbox"
+            // id="chk-toggle"
+            className="chk-toggle"
+          />
+          <div className="slider-container">
+            <span className="slider"></span>
+          </div>
+        </label>
+      </div>
 
       <Routes>
         <Route path="/" element={<Homepage />} />
@@ -27,6 +39,7 @@ function App() {
         <Route path="/product-page" element={<ProductListing />} />
         <Route path="/cart-page" element={<Cart />} />
         <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
 
     </div>
