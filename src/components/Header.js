705c9b7c101b51d2d93e1ddfb526c3fe98559de7
Al Hera Ahmad <alheraahmad0786@gmail.com>
2023-03-09 14:35:21 +0530

    feat : added wishlist page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { images } from '../constants';
 import { useCart } from '../context/Context';
 import "./styles.css";
 const Header = () => {
-    const {cartState:{cart, wishlist}} = useCart();
+    const { cartState: { cart, wishlist } } = useCart();
     return (
         <>
             <div className="navigation full-width">
@@ -19,17 +20,19 @@ const Header = () => {
                 </div>
 
                 <div className="nav-right">
-                        <Link to="/wishlist" className="icon-badge-container" >
-                            <i className="fas fa-heart" title="wishlist"><span className="icon-badge">
-                                {wishlist.length>0?wishlist.length:0} 
-                                </span></i>
-                        </Link>
-                        <Link to="/cart-page" className="icon-badge-container gutter-hz-sm" >
-                            <i className="fas fa-shopping-cart" title="cart"><span className="icon-badge">
-                                {cart.length>0?cart.length:0} 
-                            </span></i>
-                        </Link>
-                    <button className="btn btn-primary-outline text-rg-size">Login</button>
+                    <Link to="/wishlist" className="icon-badge-container" >
+                        <i className="fas fa-heart" title="wishlist"><span className="icon-badge">
+                            {wishlist.length > 0 ? wishlist.length : 0}
+                        </span></i>
+                    </Link>
+                    <Link to="/cart-page" className="icon-badge-container gutter-hz-sm" >
+                        <i className="fas fa-shopping-cart" title="cart"><span className="icon-badge">
+                            {cart.length > 0 ? cart.length : 0}
+                        </span></i>
+                    </Link>
+                    <Link to="/profile">
+                        <button className="btn btn-primary-outline text-rg-size">Profile</button>
+                    </Link>
                 </div>
             </div>
         </>
--- src/pages/Wishlist.js
+++ src/pages/Wishlist.js
@@ -1,9 +1,21 @@
 import React from 'react'
-// import { SingleCartItem } from "../components/SingleCartItem";
+import {useCart} from "../context/Context";
+import { WishlistSingleProduct } from '../components/WishlistSingleProduct';
 const Wishlist = () => {
+  const {cartState:{wishlist}} = useCart();
+  console.log({wishlist});
   return (
-    <div>
-      {/* <SingleCartItem /> */}
+    <div className="flex-column align-center">
+      {
+        wishlist.length === 0 && <h2>Wishlist is empty!</h2>
+      }
+      {
+        wishlist.length > 0 && wishlist.map((wishprd)=>{
+          return(
+            <WishlistSingleProduct product={wishprd}/>
+          )
+        })
+      }
     </div>
   )
 }
